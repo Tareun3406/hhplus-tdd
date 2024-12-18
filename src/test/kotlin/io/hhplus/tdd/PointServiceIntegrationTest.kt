480d6bc34860ee777b51586ae7a10c6787b65962
Tareun3406 <tareun3406@gmail.com>
2024-12-18 23:34:22 +0900

    feat: 동시성 통합 테스트 추가

---
+++ src/test/kotlin/io/hhplus/tdd/PointServiceIntegrationTest.kt
@@ -0,0 +1,81 @@
+package io.hhplus.tdd
+
+import io.hhplus.tdd.database.PointHistoryTable
+import io.hhplus.tdd.database.UserPointTable
+import io.hhplus.tdd.point.PointRepository
+import io.hhplus.tdd.point.PointService
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+
+class PointServiceIntegrationTest {
+    private val userPointTable = UserPointTable()
+    private val pointHistoryTable = PointHistoryTable()
+    private val pointRepository = PointRepository(userPointTable, pointHistoryTable)
+    private val pointService = PointService(pointRepository)
+
+    // ExecutorService 를 이용한 비동기 테스트
+    @Test
+    @DisplayName("동시에 충전, 사용 요청이 들어왔을경우 순차적으로 처리되어야 합니다.")
+    fun shouldProcessChargeAndUsageRequestsSequentially() {
+        // given
+        val threadCount = 5
+        val countDownLatch = CountDownLatch(threadCount)
+        val executor = Executors.newFixedThreadPool(threadCount) // 동시성 테스트를 위해 멀티 쓰레드 사용.
+
+        val task = Runnable {
+            try {
+                pointService.chargePoint(0, 100)
+                pointService.chargePoint(1, 100)
+                pointService.usePoint(1, 100)
+                pointService.usePoint(0, 100)
+                pointService.chargePoint(1, 100)
+                pointService.usePoint(1, 100)
+            } finally {
+                countDownLatch.countDown()
+            }
+        }
+
+        // when
+        for (i: Int in 0 .. threadCount) {
+            executor.submit(task)
+        }
+        countDownLatch.await()
+
+        // then
+        Assertions.assertEquals(0, pointService.getUserPoint(0).point)
+        Assertions.assertEquals(0, pointService.getUserPoint(1).point)
+    }
+
+
+    // CompletableFuture 를 사용한 비동기 테스트
+    @Test
+    @DisplayName("동시에 충전, 사용 요청이 들어왔을경우 순차적으로 처리되어야 합니다.")
+    fun shouldProcessChargeAndUsageRequestsSequentiallyTestWithAsync() {
+        // given
+        val futures = mutableListOf<CompletableFuture<Void>>()
+        val taskTimes = 5
+        val task = Runnable {
+            pointService.chargePoint(0, 100)
+            pointService.chargePoint(1, 100)
+            pointService.usePoint(1, 100)
+            pointService.usePoint(0, 100)
+            pointService.chargePoint(1, 100)
+            pointService.usePoint(1, 100)
+        }
+
+        // when
+        for (i: Int in 0 .. taskTimes) {
+            futures += CompletableFuture.runAsync(task)
+        }
+        CompletableFuture.allOf(*futures.toTypedArray()).join() // 모든 작업이 끝날 때까지 대기
+
+        // then
+        Assertions.assertEquals(0, pointService.getUserPoint(0).point)
+        Assertions.assertEquals(0, pointService.getUserPoint(1).point)
+    }
+}
